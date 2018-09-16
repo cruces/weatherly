1026a587899acf713b61326246d9f82a34185adf
Dumevi Cruces <dcs.dume@gmail.com>
2018-09-16 22:56:56 +0100

    Change icons directories

--- app/src/main/java/com/weatherly/weatherly/modules/common/openweathermap/IconUtils.java
+++ app/src/main/java/com/weatherly/weatherly/modules/common/openweathermap/IconUtils.java
@@ -10,61 +10,61 @@ public class IconUtils {
     public static Drawable getWeatherIcon(Context context, String code) {
         switch (code) {
             case "04d":
-                return ContextCompat.getDrawable(context, R.mipmap.cloudyn);
+                return ContextCompat.getDrawable(context, R.drawable.cloudyn);
 
             case "04n":
-                return ContextCompat.getDrawable(context, R.mipmap.cloudyn);
+                return ContextCompat.getDrawable(context, R.drawable.cloudyn);
 
             case "01d":
-                return ContextCompat.getDrawable(context, R.mipmap.cleard);
+                return ContextCompat.getDrawable(context, R.drawable.cleard);
 
             case "01n":
-                return ContextCompat.getDrawable(context, R.mipmap.clearn);
+                return ContextCompat.getDrawable(context, R.drawable.clearn);
 
             case "02d":
-                return ContextCompat.getDrawable(context, R.mipmap.fewcloudd);
+                return ContextCompat.getDrawable(context, R.drawable.fewcloudd);
 
             case "02n":
-                return ContextCompat.getDrawable(context, R.mipmap.fewcloudn);
+                return ContextCompat.getDrawable(context, R.drawable.fewcloudn);
 
             case "03d":
-                return ContextCompat.getDrawable(context, R.mipmap.scatteredd);
+                return ContextCompat.getDrawable(context, R.drawable.scatteredd);
 
             case "03n":
-                return ContextCompat.getDrawable(context, R.mipmap.scatteredd);
+                return ContextCompat.getDrawable(context, R.drawable.scatteredd);
 
             case "09d":
-                return ContextCompat.getDrawable(context, R.mipmap.showerd);
+                return ContextCompat.getDrawable(context, R.drawable.showerd);
 
             case "09n":
-                return ContextCompat.getDrawable(context, R.mipmap.showerd);
+                return ContextCompat.getDrawable(context, R.drawable.showerd);
 
             case "10d":
-                return ContextCompat.getDrawable(context, R.mipmap.raind);
+                return ContextCompat.getDrawable(context, R.drawable.raind);
 
             case "10n":
-                return ContextCompat.getDrawable(context, R.mipmap.rainn);
+                return ContextCompat.getDrawable(context, R.drawable.rainn);
 
             case "11d":
-                return ContextCompat.getDrawable(context, R.mipmap.thunderd);
+                return ContextCompat.getDrawable(context, R.drawable.thunderd);
 
             case "11n":
-                return ContextCompat.getDrawable(context, R.mipmap.thunderd);
+                return ContextCompat.getDrawable(context, R.drawable.thunderd);
 
             case "12d":
-                return ContextCompat.getDrawable(context, R.mipmap.snowd);
+                return ContextCompat.getDrawable(context, R.drawable.snowd);
 
             case "12n":
-                return ContextCompat.getDrawable(context, R.mipmap.snowd);
+                return ContextCompat.getDrawable(context, R.drawable.snowd);
 
             case "13d":
-                return ContextCompat.getDrawable(context, R.mipmap.mistd);
+                return ContextCompat.getDrawable(context, R.drawable.mistd);
 
             case "13n":
-                return ContextCompat.getDrawable(context, R.mipmap.mistd);
+                return ContextCompat.getDrawable(context, R.drawable.mistd);
 
             default:
-                return ContextCompat.getDrawable(context, R.mipmap.cloudyn);
+                return ContextCompat.getDrawable(context, R.drawable.cloudyn);
         }
     }
 }
